383537aacf720b8d902746e01c033b94190afd35
Stanislav Romanov <kaermorchen@gmail.com>
2017-12-05 21:56:39 +0300

    Added signalr npm package

--- index.js
+++ index.js
@@ -2,5 +2,14 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-signalr'
+  name: 'ember-signalr',
+
+  included: function (app) {
+    this._super.included(app);
+
+    app.import({
+      development: path.join('node_modules', 'signalr', 'jquery.signalR.js'),
+      production:  path.join('node_modules', 'signalr', 'jquery.signalR.min.js'),
+    });
+  },
 };
